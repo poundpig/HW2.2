822f423352f62fedb5db85afb1a91d26a69901fb
Denis Fedorov <poundpig@yandex.ru>
2020-08-24 12:03:54 +0300

    rrr

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var rSlider: UISlider! {
+    didSet{
+        rSlider.tintColor = UIColor.red
+    }
+    }
+    @IBOutlet weak var gSlider: UISlider! {
+    didSet{
+        gSlider.tintColor = UIColor.green
+    }
+    }
+    @IBOutlet weak var bSlider: UISlider! {
+    didSet{
+        bSlider.tintColor = UIColor.blue
+    }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(rSlider.value), green: CGFloat(gSlider.value), blue: CGFloat(bSlider.value), alpha: 1)
+    }
+    
+    @IBAction func rgbSliderChanged(_ sender: Any) {
+        changeColor()
+    }
+    
 }
 
